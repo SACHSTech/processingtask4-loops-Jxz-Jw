31b22d2f1a977512e99f138dc72477808bd9ec31
Jxz.Jw <jasmine.woo23@ycdsbk12.ca>
2022-03-30 11:02:04 -0400

    hehe

--- Sketch.java
+++ Sketch.java
@@ -23,14 +23,66 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    //quadrant 1
+    for(int intLineX = 0; intLineX <= 200; intLineX += 20){
+      fill(0);
+      line(intLineX, 0, intLineX, height/2);
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    for(int intLineY = 0; intLineY <= 200; intLineY += 20){
+      fill(0);
+      line(0, intLineY, width/2, intLineY);
+    }
+
+    //quadrant 2 (10x10 grid)
+    //Row 1
+    for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){ 
+      fill(255, 0, 200);
+      ellipse(intCircleX, (float)(height/25), (float)(width/20), (float)(height/20));
+    }
+
+    //Row 2 (5 rows of 5 circles)
+    for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
+      fill(255, 0, 200);
+      ellipse(intCircleX, (float)(height/7.8), (float)(width/20), (float)(height/20));
+    }
+
+    //Row 3
+    for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
+      fill(255, 0, 200);
+      ellipse(intCircleX, (float)(height/4.5), (float)(width/20), (float)(height/20));
+    }
+
+    //Row 4
+    for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
+      fill(255, 0, 200);
+      ellipse(intCircleX, (float)(height/3.2), (float)(width/20), (float)(height/20));
+    }
+
+    //Row 5
+    for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
+      fill(255, 0, 200);
+      ellipse(intCircleX, (float)(height/2.48), (float)(width/20), (float)(height/20));
+    }
+
+    //Quadrant 3 (gradient)
+
+    for(int intColour = 0; intColour <= 200; intColour ++){
+      stroke(intColour);
+      line(intColour, height/2, intColour, height);
+    }
+
+    /*int intLines = 0;
+    int intColour = 0;
+
+    while(intLines <= 200){
+      intLines = intLines + 1;
+      intColour = intColour + 1;
+    
+      stroke(intColour);
+      line(intLines, height/2, intLines, height);
+    }*/
+ 
   }
-  
   // define other methods down here.
 }
