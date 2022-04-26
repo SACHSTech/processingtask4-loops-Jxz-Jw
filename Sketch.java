44d11e7952d97aa94252a2e8ed7979193eddb22d
your_github_username <your_k12_emails>
2022-04-26 00:20:49 -0400

    complete

--- Sketch.java
+++ Sketch.java
@@ -1,11 +1,15 @@
+/**
+* A drawing of four quadrants, using loops to create four different patterns.
+* @author: J.Woo
+*
+*/
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
   /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  * Called once at the beginning of execution, put your size all in this method
+  */
   public void settings() {
 	// put your size call here
     size(400, 400);
@@ -23,66 +27,88 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-    //quadrant 1
+    
+    stroke(0);
+    
+    //Quadrant 1 (10x10 grid)
+    
+    //Column lines
     for(int intLineX = 0; intLineX <= 200; intLineX += 20){
       fill(0);
       line(intLineX, 0, intLineX, height/2);
     }
 
+    //Row lines
     for(int intLineY = 0; intLineY <= 200; intLineY += 20){
       fill(0);
       line(0, intLineY, width/2, intLineY);
     }
 
-    //quadrant 2 (10x10 grid)
+    //Quadrant 2 (5x5 row of circles)
+
     //Row 1
     for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){ 
-      fill(255, 0, 200);
-      ellipse(intCircleX, (float)(height/25), (float)(width/20), (float)(height/20));
+      fill(255, 0, 179);
+      ellipse(intCircleX, (float)(height/19.8), (float)(width/20), (float)(height/20));
     }
 
-    //Row 2 (5 rows of 5 circles)
+    //Row 2
     for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
-      fill(255, 0, 200);
-      ellipse(intCircleX, (float)(height/7.8), (float)(width/20), (float)(height/20));
+      fill(255, 0, 179);
+      ellipse(intCircleX, (float)(height/6.5), (float)(width/20), (float)(height/20));
     }
 
     //Row 3
     for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
-      fill(255, 0, 200);
-      ellipse(intCircleX, (float)(height/4.5), (float)(width/20), (float)(height/20));
+      fill(255, 0, 179);
+      ellipse(intCircleX, (float)(height/3.96), (float)(width/20), (float)(height/20));
     }
 
     //Row 4
     for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
-      fill(255, 0, 200);
-      ellipse(intCircleX, (float)(height/3.2), (float)(width/20), (float)(height/20));
+      fill(255, 0, 179);
+      ellipse(intCircleX, (float)(height/2.82), (float)(width/20), (float)(height/20));
     }
 
     //Row 5
     for(int intCircleX = 220; intCircleX <= 380; intCircleX += 40){
-      fill(255, 0, 200);
-      ellipse(intCircleX, (float)(height/2.48), (float)(width/20), (float)(height/20));
+      fill(255, 0, 179);
+      ellipse(intCircleX, (float)(height/2.22), (float)(width/20), (float)(height/20));
     }
-
-    //Quadrant 3 (gradient)
+        
+    //Quadrant 3 (grayscale gradient)
 
     for(int intColour = 0; intColour <= 200; intColour ++){
       stroke(intColour);
       line(intColour, height/2, intColour, height);
     }
 
-    /*int intLines = 0;
-    int intColour = 0;
+    //Quadrant 4 (8-petal flower)
 
-    while(intLines <= 200){
-      intLines = intLines + 1;
-      intColour = intColour + 1;
+    // Change outline to black
+    stroke(12,12,12);
     
-      stroke(intColour);
-      line(intLines, height/2, intLines, height);
-    }*/
- 
+    // Set petal colour to orange
+    fill(240,128,0);
+
+    // Translate the point to the center of the fourth quadrant
+    translate(width * (float)0.75, height * (float) 0.75);
+    
+    // Loop for petals
+    for(int intPetal = 0; intPetal < 8; intPetal++){
+
+    // Rotate petals
+    rotate(PI/4);
+
+    // Create petals
+    ellipse(0,0, width / 17 , height / (float)2.8);
+    }
+    noStroke();
+
+    // Set the colour of the centre of the flower + creating it
+    fill(45,180,78);
+    ellipse(0,0,width/10,height/10);
+
   }
-  // define other methods down here.
+  
 }
